f8898ba94c315239ba315a4e4fefde1bb57d3277
Aster <galaster@foxmail.com>
2019-01-21 18:58:06 +0800

    算法::获取可选择的行动

--- Packages/Bor-Bor Zan.m
+++ Packages/Bor-Bor Zan.m
@@ -1,10 +1,49 @@
 (* ::Package:: *)
 
 (* ::Section:: *)
-(*State Transition*)
+(*Auxiliary Functions*)
 
 
 (* ::Subsection:: *)
+(*Usage*)
+
+
+V::usage="V[i,j] \:8868\:793a\:6211\:65b9 i \:6c14, \:654c\:65b9 j \:6c14\:65f6\:7684\:80dc\:7387\:8bc4\:4f30";
+ExpandV::usage="ExpandV \:80fd\:5c55\:5f00\:652f\:4ed8\:77e9\:9635";
+
+
+(* ::Subsection:: *)
+(*Definition  *)
+
+
+ExpandV[e__] := Block[
+	{eval, V},
+	eval[i_, j_] := Which[
+		j == i, {1 / 2, 1 / 2},
+		j > i, {1 - V[i, j], V[i, j]},
+		True, {V[i, j], 1 - V[i, j]}
+	];
+	Return[e /. V -> eval]
+];
+getAction[V[i_,j_]]:=Outer[List,getAction[i],getAction[j]]
+getAction[energy_Integer] := Block[
+	{slot, rm},
+	slot = {Overkill, Dodge, Rebound, Skill, Defense, Attack, Power};
+	rm[ops_] := DeleteCases[slot, Alternatives @@ ops];
+	Which[
+		energy >= 5, slot,
+		energy >= 3, rm@{Overkill},
+		energy >= 1, rm@{Overkill, Skill},
+		energy == 0, {Power, Defense}
+	]
+];
+
+
+(* ::Section:: *)
+(*State Transition*)
+
+
+(* ::Subsection::Closed:: *)
 (*Definition  *)
 
 
